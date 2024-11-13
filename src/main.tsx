93353a08d22a3f63c25974401c1c014d367b993c
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-11-13 16:49:02 +0100

    feat: refactor the code

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApiProvider, createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+interface PokemonListing {
+  count: number;
+  results: Array<{
+    name: string;
+    url: string;
+  }>;
+}
 
 const api = createApi({
-  baseQuery: () => {},
+  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
   endpoints: (build) => ({
-    pokemonList: build.query({
-      queryFn() {
-        return { data: fakePokemonListing };
+    pokemonList: build.query<PokemonListing, void>({
+      query() {
+        return {
+          url: "pokemon",
+          params: {
+            limit: 9,
+          },
+          method: "GET",
+        };
       },
     }),
     pokemonDetails: build.query({
-      queryFn() {
-        return { data: fakePokemonDetailData };
+      query({ name }: string | number | any) {
+        return `pokemon/${name}/`;
       },
     }),
   }),
@@ -20,56 +36,28 @@ const api = createApi({
 
 const { usePokemonListQuery, usePokemonDetailsQuery } = api;
 
-// data copy & pasted from https://pokeapi.co/api/v2/pokemon?limit=9
-const fakePokemonListing = {
-  count: 1126,
-  next: "https://pokeapi.co/api/v2/pokemon?offset=9&limit=9",
-  previous: null,
-  results: [
-    { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
-    { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
-    { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
-    { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
-    { name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" },
-    { name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/" },
-    { name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/" },
-    { name: "wartortle", url: "https://pokeapi.co/api/v2/pokemon/8/" },
-    { name: "blastoise", url: "https://pokeapi.co/api/v2/pokemon/9/" },
-  ],
-};
-// partial data of https://pokeapi.co/api/v2/pokemon/1/
-const fakePokemonDetailData = {
-  id: 1,
-  name: "bulbasaur",
-  height: 7,
-  weight: 69,
-  types: [
-    {
-      slot: 1,
-      type: { name: "grass", url: "https://pokeapi.co/api/v2/type/12/" },
-    },
-    {
-      slot: 2,
-      type: { name: "poison", url: "https://pokeapi.co/api/v2/type/4/" },
-    },
-  ],
-  sprites: {
-    front_default:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
   },
-};
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(api.middleware);
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <ApiProvider api={api}>
+    <Provider store={store}>
       <App />
-    </ApiProvider>
+    </Provider>
   </React.StrictMode>
 );
 
 function App() {
-  const [selectedPokemon, selectPokemon] = React.useState(undefined);
+  const [selectedPokemon, selectPokemon] = React.useState<string | undefined>(
+    undefined
+  );
 
   return (
     <>
@@ -91,34 +79,33 @@ function App() {
 }
 
 type Props = {
-  onPokemonSelected: any;
+  onPokemonSelected: (name: string) => void;
 };
 
 function PokemonList({ onPokemonSelected }: Props) {
-  const { data, isLoading, isError, isSuccess } = usePokemonListQuery();
+  const { data, isLoading, isError, isUninitialized } = usePokemonListQuery();
 
-  if (isLoading) {
+  if (isLoading || isUninitialized) {
     return "Loading...";
   }
   if (isError) {
     return "Something went wrong...";
   }
-  if (isSuccess) {
-    return (
-      <article>
-        <h2>Overview</h2>
-        <ol start={1}>
-          {data.results.map((pokemon: any) => (
-            <li key={pokemon.name}>
-              <button onClick={() => onPokemonSelected(pokemon.name)}>
-                {pokemon.name}
-              </button>
-            </li>
-          ))}
-        </ol>
-      </article>
-    );
-  }
+
+  return (
+    <article>
+      <h2>Overview</h2>
+      <ol start={1}>
+        {data.results.map((pokemon: any) => (
+          <li key={pokemon.name}>
+            <button onClick={() => onPokemonSelected(pokemon.name)}>
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
+      </ol>
+    </article>
+  );
 }
 
 const listFormatter = new Intl.ListFormat("en-GB", {
@@ -130,29 +117,32 @@ type NameProps = {
   pokemonName: string;
 };
 function PokemonDetails({ pokemonName }: NameProps) {
-  const { data, isLoading, isError, isSuccess } = usePokemonDetailsQuery();
+  const { data, isLoading, isError, isUninitialized } = usePokemonDetailsQuery({
+    name: pokemonName,
+  });
 
-  if (isLoading) {
+  if (isLoading || isUninitialized) {
     return "Loading...";
   }
   if (isError) {
     return "Something went wrong...";
   }
-  if (isSuccess) {
-    return (
-      <article>
-        <h2>{data?.name}</h2>
-        <img src={data?.sprites?.front_default} alt={data?.name} />
-        <ul>
-          <li>id: {data?.id}</li>
-          <li>height: {data?.height}</li>
-          <li>weight: {data?.weight}</li>
-          <li>
-            types:
-            {listFormatter.format(data?.types?.map((item:any) => item?.type?.name))}
-          </li>
-        </ul>
-      </article>
-    );
-  }
+
+  return (
+    <article>
+      <h2>{data?.name}</h2>
+      <img src={data?.sprites?.front_default} alt={data?.name} />
+      <ul>
+        <li>id: {data?.id}</li>
+        <li>height: {data?.height}</li>
+        <li>weight: {data?.weight}</li>
+        <li>
+          types:
+          {listFormatter.format(
+            data?.types?.map((item: any) => item?.type?.name)
+          )}
+        </li>
+      </ul>
+    </article>
+  );
 }
